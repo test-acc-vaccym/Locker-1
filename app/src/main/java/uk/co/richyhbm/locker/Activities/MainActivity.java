1fcb7e38d38dc5eacb9a3d999e18f1eeb1a9f4b2
RichyHBM <gitemail@richyhbm.co.uk>
2018-02-08 20:31:18 +0000

    Add about page

--- app/src/main/java/uk/co/richyhbm/locker/Activities/MainActivity.java
+++ app/src/main/java/uk/co/richyhbm/locker/Activities/MainActivity.java
@@ -50,6 +50,8 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case R.id.action_about:
+                Intent aboutIntent = new Intent(this, AboutActivity.class);
+                startActivity(aboutIntent);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
