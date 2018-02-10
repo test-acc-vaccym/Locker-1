59b6fc5037719aa9d71a0ca20289b3f540590fe9
RichyHBM <gitemail@richyhbm.co.uk>
2018-02-10 17:01:59 +0000

    More settings

--- app/src/main/java/uk/co/richyhbm/locker/Utilities/Settings.java
+++ app/src/main/java/uk/co/richyhbm/locker/Utilities/Settings.java
@@ -5,31 +5,50 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import uk.co.richyhbm.locker.R;
+
 public class Settings {
     private Context context;
-    private SharedPreferences settings;
+    private SharedPreferences sharedPreferences;
 
     public Settings(Context context) {
         this.context = context;
-        this.settings = PreferenceManager.getDefaultSharedPreferences(context);
+        this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
     private String getResString(int resId) { return context.getString(resId); }
-    private int getResInt(int resId) { return context.getResources().getInteger(resId); }
-    private boolean getBoolean(int keyId, boolean defaultValue) { return settings.getBoolean(getResString(keyId), defaultValue); }
-    private int getInt(int keyId, int defaultId) { return settings.getInt(getResString(keyId), getResInt(defaultId)); }
-    private int getIntValue(int keyId, int defaultValue) { return settings.getInt(getResString(keyId), defaultValue); }
-    private long getLong(int keyId, long defaultValue) { return settings.getLong(getResString(keyId), defaultValue); }
+    public String getString(int keyId, String defaultValue) { return sharedPreferences.getString(getResString(keyId), defaultValue); }
+    private boolean getBoolean(int keyId, boolean defaultValue) { return sharedPreferences.getBoolean(getResString(keyId), defaultValue); }
+    private int getInt(int keyId, int defaultValue) { return sharedPreferences.getInt(getResString(keyId), defaultValue); }
 
     private void setBoolean(int keyId, boolean value) {
-        settings.edit().putBoolean(getResString(keyId), value).apply();
+        sharedPreferences.edit().putBoolean(getResString(keyId), value).apply();
     }
 
     private void setInt(int keyId, int value) {
-        settings.edit().putInt(getResString(keyId), value).apply();
+        sharedPreferences.edit().putInt(getResString(keyId), value).apply();
     }
 
     private void remove(int keyId) {
-        settings.edit().remove(getResString(keyId)).apply();
+        sharedPreferences.edit().remove(getResString(keyId)).apply();
+    }
+
+    public boolean isInSafeMode() { return getBoolean(R.string.settings_key_safe_mode, true); }
+    public boolean shouldPersistFailsAfterReboot() { return getBoolean(R.string.settings_key_safe_mode, true); }
+
+    public int getMaxFailedAttemptsForWipe() { return Integer.parseInt(getString(R.string.settings_key_max_failed_logins_for_wipe, "0")); }
+    public int currentFailedAttempts() { return getInt(R.string.settings_key_failed_logins_count, 0); }
+    public void addToCurrentFailedAttempts() {
+        setInt(R.string.settings_key_failed_logins_count,
+                getInt(R.string.settings_key_failed_logins_count, 0) + 1
+        );
+    }
+
+    public void resetCurrentFailedAttempts() {
+        setInt(R.string.settings_key_failed_logins_count, 0);
+    }
+
+    public boolean wipeExternalStorage() {
+        return getBoolean(R.string.settings_key_wipe_external_storage, false);
     }
 }
